ad21cdd6ffbf7942d325136a808c8f01d04464dd
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2021-06-28 17:30:14 -0300

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+const axios = require('axios')
 const express = require('express');
+const schedule = require('node-schedule');
+
 const app = express();
+const newsApiRoute = 'http://newsapi.org/v2/everything?q=rich&sortBy=publishedAt&apiKey=';
+const newsApiKey = '6633de5b30b74366b611995131df0058';
+
 let port = process.env.PORT || 3000;
+
+//array que gaurda la apicall
+let newsData = [];
 
 app.get("/", (req,res) =>{
     res.send("Hello, World");
 });
+
+//hace el trabajo, recibe la llamada del navegador o tercero a mi servidor y devuelve newData
+router.get("/newsapi", (req, res) => {
+    res.send(newsData);
+});
+
+const getNewsJob = schedule.scheduleJob('31 * * * *', function () {
+    console.log('Trae las noticias en el minuto N de la hora')
+    axios.get(`${newsApiRoute}${newsApiKey}`)
+      .then((response) => newsData=response.data)
+      .catch((error) => console.log(error)); 
+});
+
+
 
 app.listen(port, () =>{
     console.log(`example app is listening on port http://localhost:${port}`);
