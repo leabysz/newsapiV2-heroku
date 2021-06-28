81dc5ac93e0d592b4c0acdf6646d14efcb6866e0
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2021-06-28 18:48:57 -0300

    make it better

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const express = require('express');
 const schedule = require('node-schedule');
 
 const app = express();
-const newsApiRoute = 'http://newsapi.org/v2/everything?q=rich&sortBy=publishedAt&apiKey=';
+
+//documentacion https://newsapi.org/docs/endpoints/everything
+const newsApiRoute = 'https://newsapi.org/v2/everything?';
+//para agregar poner OR y usar + en lugar de espacios
+const newsApiKeywords = 'crypto+OR+bitcoin+OR+cryptocurrency';
+//opciones: relevancy, popularity, publishedAt
+const newsApiSortBy = 'publishedAt';
+//cantidad de articulos, maximo 100
+const newsApiSize = '10';
 const newsApiKey = '6633de5b30b74366b611995131df0058';
 
-let port = process.env.PORT || 3000;
-
-//array que gaurda la apicall
+//array que guarda la apicall
 let newsData = [];
+let port = process.env.PORT || 3000;
 
 app.get("/", (req,res) =>{
     res.send("Hello, World");
@@ -20,15 +27,20 @@ app.get("/newsapi", (req, res) => {
     res.send(newsData);
 });
 
-const getNewsJob = schedule.scheduleJob('36 * * * *', function () {
-    console.log('Trae las noticias en el minuto N de la hora')
-    axios.get(`${newsApiRoute}${newsApiKey}`)
+//console.log de la direccion api, en caso de cambios siempre verificar 
+//entrando al link de la console log que funcione correctamente
+const direcccionAPi = `${newsApiRoute}q=${newsApiKeywords}&sortBy=${newsApiSortBy}&pageSize=${newsApiSize}&apiKey=${newsApiKey}`;
+console.log(direcccionAPi)
+
+//realiza un fetch a la api cada 15 minutos. Cambiando por 30, seria cada 30 minutos.
+//quitando el primer */, se activara en el minuto N de cada hora. Documentacion schedule-node
+const getNewsJob = schedule.scheduleJob('*/15 * * * *', function () {
+    console.log('getNewsJob activado, trayendo nuevas noticias')
+    axios.get(direcccionAPi)
       .then((response) => newsData=response.data)
       .catch((error) => console.log(error)); 
 });
 
-
-
 app.listen(port, () =>{
-    console.log(`example app is listening on port http://localhost:${port}`);
+    console.log(`app funcionado en el puerto http://localhost:${port}`);
 });
