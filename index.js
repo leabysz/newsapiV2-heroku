21fb4ed2e633a7776024a5aee7a8e965c9887e61
Bruno Paulette <brunopaulettecurbelo@gmail.com>
2021-06-29 12:22:46 -0300

    headers

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const newsApiKey = '6633de5b30b74366b611995131df0058';
 let newsData = [];
 let port = process.env.PORT || 3000;
 
+
 app.get("/", (req,res) =>{
     res.send("Hello, World");
 });
@@ -30,11 +31,11 @@ app.get("/newsapi", (req, res) => {
 //console.log de la direccion api, en caso de cambios siempre verificar 
 //entrando al link de la console log que funcione correctamente
 const direcccionAPi = `${newsApiRoute}q=${newsApiKeywords}&sortBy=${newsApiSortBy}&pageSize=${newsApiSize}&apiKey=${newsApiKey}`;
-console.log(direcccionAPi)
+console.log(direcccionAPi);
 
 //realiza un fetch a la api cada 15 minutos. Cambiando por 30, seria cada 30 minutos.
 //quitando el primer */, se activara en el minuto N de cada hora. Documentacion schedule-node
-const getNewsJob = schedule.scheduleJob('*/15 * * * *', function () {
+const getNewsJob = schedule.scheduleJob('*/1 * * * *', function () {
     console.log('getNewsJob activado, trayendo nuevas noticias')
     axios.get(direcccionAPi)
       .then((response) => newsData=response.data)
